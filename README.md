4cb3e2d9d320efaf9fe3ed88b2dea94d6bf0c042
Vaibhav Dhoke <vaibhavdhoke1@gmail.com>
2020-05-28 01:04:49 -0400

    Adding web dev project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # webapp
+
+1.Clone/Download the repo.
+2.Run npm install.
+3.Run npm start to spin up the local dev server port 5000.(http://localhost:5000).
+4 Run npm test for testing the web application
+
---
+++ routes/pages.js
@@ -0,0 +1,44 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/",(req,res)=>{
+    res.status(200);
+    res.render("login");
+});
+
+router.get("/register",(req,res)=>{
+    res.render("register");
+});
+
+router.get("/changePassword",(req,res)=>{
+    res.render("changePassword");
+});
+
+router.get("/login",(req,res)=>{
+    res.render("login");
+});
+
+router.get("/updateprofile/:id",(req,res)=>{
+    console.log(req.params.id);
+    
+    return res.render("updateprofile",{
+        id:req.params.id
+    });
+});
+
+
+router.get("/logout",(req,res)=>{
+    console.log('Hi'+req.session.user);
+    req.session.destroy(err=>{
+       if(err){
+           return res.redirect('/home');
+       }
+       res.clearCookie('cookie');
+       res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+       res.redirect('/'); 
+    })
+    // console.log(req.session.user);
+});
+
+module.exports = router;
