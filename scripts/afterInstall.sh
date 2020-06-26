2efbdfcee00e7ea25c9d6cd01912c181f48fc4a4
Vaibhav Dhoke <vaibhavdhoke1@gmail.com>
2020-06-26 16:36:36 -0400

    Copy config file

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -fR /home/ubuntu/webapp/config /home/ubuntu/csye6225-webapp/webapp
+cp -a /home/ubuntu/webapp/config/. /home/ubuntu/csye6225-webapp/webapp/config
 cd /home/ubuntu/csye6225-webapp/webapp
 npm install forever -g
 npm install
