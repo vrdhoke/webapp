d64af32e74063d079ff810996d550f549d4e938c
Vaibhav Dhoke <vaibhavdhoke1@gmail.com>
2020-06-26 17:03:45 -0400

    Changes in afterinstall file

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cp -a /home/ubuntu/webapp/config/. /home/ubuntu/csye6225-webapp/webapp/config
+rm -r /home/ubuntu/csye6225-webapp/webapp/config
+cp -r /home/ubuntu/webapp/config /home/ubuntu/csye6225-webapp/webapp
 cd /home/ubuntu/csye6225-webapp/webapp
 npm install forever -g
 npm install
