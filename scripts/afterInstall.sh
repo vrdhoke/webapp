97fcda21c029548716a6f27bf50b184148c8f25b
Vaibhav Dhoke <vaibhavdhoke1@gmail.com>
2020-06-26 16:29:20 -0400

    Config file copied from userdata

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mv /home/ubuntu/webapp/config /home/ubuntu/csye6225-webapp/webapp
+cp -fR /home/ubuntu/webapp/config /home/ubuntu/csye6225-webapp/webapp
 cd /home/ubuntu/csye6225-webapp/webapp
 npm install forever -g
 npm install
