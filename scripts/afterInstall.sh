55d4fafe426e5eb74465cac8067b3c9414674385
Vaibhav Dhoke <vaibhavdhoke1@gmail.com>
2020-07-06 22:24:31 -0400

    Added changes for system reboot

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -5,6 +5,8 @@ sudo rm -r /home/ubuntu/csye6225-webapp/webapp/config
 sudo cp -r /home/ubuntu/webapp/config /home/ubuntu/csye6225-webapp/webapp
 cd /home/ubuntu/csye6225-webapp/webapp
 sudo npm install forever -g
+sudo npm install -g forever-service
 sudo npm install
+sudo forever-service install test
 
 
